1195e1df2a40f794f01339f0b138f1103e0e5355
Joao Stopiglia <joao.stopiglia@aluno.senai.br>
2025-04-03 17:02:36 -0300

    feat(if-else): adiciona classificação etária por idade

---
+++ If_Else/classificacaoetaria.js
@@ -0,0 +1,11 @@
+let idade = 70
+
+if (idade > 0 && idade < 12) {
+    console.log("criança")
+} else if (idade > 13 && idade < 17) {
+    console.log ("adolescente")
+} else if (idade > 18 && idade < 59) {
+    console.log("adulto")
+} else {
+    console.log("idoso")
+}
