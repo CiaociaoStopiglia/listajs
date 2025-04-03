d14bef5f67c545a4f7322a6a09794160ef3019b4
Joao Stopiglia <joao.stopiglia@aluno.senai.br>
2025-04-03 16:55:34 -0300

    feat(if-else): adiciona comparação do maior entre dois números

---
+++ If_Else/onumeromaiorvence.js
@@ -0,0 +1,10 @@
+let numero1 = 12
+let numero2 = 10
+
+if (numero1 > numero2) {
+    console.log("o maior número é o número 1")
+} else if (numero2 > numero1) {
+    console.log("o número 2 é maior que o número 1")
+} else {
+    console.log("os números são iguais")
+}
