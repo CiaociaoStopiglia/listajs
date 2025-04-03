0f3dadfbc91df62cd33f480b300497cb1aab3346
Joao Stopiglia <joao.stopiglia@aluno.senai.br>
2025-04-03 18:37:16 -0300

    feat(switch-case): adiciona exibição da estação do ano

---
+++ Switch_Case/estacaodoano.js
@@ -0,0 +1,18 @@
+let codigo = 2
+
+switch (codigo) {
+    case 1:
+        console.log("verão");
+        break;
+    case 2:
+        console.log("outono")
+        break;
+    case 3:
+        console.log("primavera");
+        break;
+    case 4:
+        console.log("inverno");
+        break;
+    default:
+        console.log("Codigo errado, insira um codigo válido.")
+}
