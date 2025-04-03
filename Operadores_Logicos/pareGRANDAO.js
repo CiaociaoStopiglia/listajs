d82eacef991a3b271bc4435b192a497402591dd1
Joao Stopiglia <joao.stopiglia@aluno.senai.br>
2025-04-03 16:19:38 -0300

    feat(operadores-logicos): adiciona verificação de par e maior que 10

---
+++ Operadores_Logicos/pareGRANDAO.js
@@ -0,0 +1,7 @@
+let numero1 = 2;
+
+if (numero1 % 2 === 0 && numero1 > 10) {
+    console.log("o numero é par e maior que 10")
+} else {
+    console.log("o numero não está dentro dos requisitos")
+}
