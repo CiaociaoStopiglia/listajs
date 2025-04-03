078db24d3156a27bacafc0267bde2713f92570a1
Joao Stopiglia <joao.stopiglia@aluno.senai.br>
2025-04-03 16:50:37 -0300

    feat(if-else): adiciona verificação de positivo, negativo ou zero

---
+++ If_Else/positivoounegativo.js
@@ -0,0 +1,9 @@
+let numero1 = 0
+
+if (numero1 > 0) {
+    console.log("numero é positivo")
+} else if (numero1 < 0) {
+    console.log("o numero é negativo")
+} else {
+    console.log("o numero é zero")
+}
