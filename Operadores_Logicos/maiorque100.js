ae519eb8fea5ac0f5fc36427188ff144c4f1a28b
Joao Stopiglia <joao.stopiglia@aluno.senai.br>
2025-04-03 16:08:16 -0300

    feat(operadores-logicos): adiciona verificação de número maior que 100

---
+++ Operadores_Logicos/maiorque100.js
@@ -0,0 +1,9 @@
+let numero1 = 10;
+let numero2 = 101;
+let numero3 = 11;
+
+if (numero1 > 100 || numero2 > 100 || numero3 > 100) {
+    console.log ("Um dos números é maior que 100!")
+} else {
+    console.log ("Nenhum dos números é maior que 100!")
+}
